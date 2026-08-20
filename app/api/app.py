6a7684c40a71b01a60d7f902fd28507c98699e07
Samridhi Shandilya <samridhishandilya118@gmail.com>
2026-08-20 23:40:29 +0000

    Add NimbusCart frontend

--- app/api/app.py
+++ app/api/app.py
@@ -3,9 +3,11 @@ import time
 
 import mysql.connector
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
